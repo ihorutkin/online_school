387a7454e673e023ac5b1a6996c8f6d255fa10ab
Ihor Utkin <igorutkin2002@gmail.com>
2024-09-25 03:08:18 +0200

    remove django secrety key for deployment

--- online_service/settings.py
+++ online_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-ag!9(9qp^2e#z#%w)g#a_z8n0_y9v-%b*%dy06#en3t6(&k_)k")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
